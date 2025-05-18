719497309403f023a8fc1eaecacb018c19595369
Bill Sanderson <bill@towniebagels.com>
2025-05-17 19:50:41 -0700

    start section 11 lesson added assets  .dart and image folder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:section11/main.dart';
+import 'package:section11/mainfcs.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
